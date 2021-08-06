6a4334ee9efbc38527c4c7db406699bef041a911
Philip Breuer <philip.breuer@pm.me>
2021-08-06 11:21:52 +0200

    Add braille support

--- webterm.rb
+++ webterm.rb
@@ -4,19 +4,34 @@ require 'curses'
 require 'ferrum'
 require 'chunky_png'
 
-def display(win, browser, width, height, top, left)
-  encoding = ' .:-=+*#%@'
+BRAILLE = false
+
+def display(win, browser, top, left)
   browser.mouse.scroll_to(top, left)
   browser.screenshot(path: 'tmp.png')
-
   image = ChunkyPNG::Image.from_file('tmp.png')
-  (0...height).each do |y|
-    (0...width).each do |x|
-      pixel = image[x, y]
-      r = ChunkyPNG::Color.r(pixel)
-      g = ChunkyPNG::Color.g(pixel)
-      b = ChunkyPNG::Color.b(pixel)
-      grayscale = (r + g + b) / 3.0
+
+  if BRAILLE
+    chars = (0...image.width * image.height / 8).map do |index|
+      cx = index * 2 % image.width
+      cy = index * 7 / image.width # TODO: Should be * 8, but it ends up out of bounds
+      pixels = [
+        image[cx + 0, cy + 0],
+        image[cx + 0, cy + 1],
+        image[cx + 0, cy + 2],
+        image[cx + 1, cy + 0],
+        image[cx + 1, cy + 1],
+        image[cx + 1, cy + 2],
+        image[cx + 0, cy + 3],
+        image[cx + 1, cy + 3],
+      ]
+      pixels.map { |pixel| (2.0 / 256.0 * ChunkyPNG::Color.grayscale_teint(pixel)).floor }.reverse.join('').to_i(2) + 0x2800
+    end
+    win.addstr(chars.pack('U*'))
+  else
+    encoding = ' .:-=+*#%@'
+    image.pixels.each do |pixel|
+      grayscale = ChunkyPNG::Color.grayscale_teint(pixel)
       index = (grayscale / 256.0 * encoding.length).floor
       win.addstr(encoding[index])
     end
@@ -31,21 +46,24 @@ begin
   win.keypad(true)
   width = win.maxx
   height = win.maxy
+  if BRAILLE
+    width *= 2
+    height *= 4
+  end
   top = 0
   left = 0
   browser = Ferrum::Browser.new(window_size: [width, height])
   browser.go_to(ARGV.last || 'https://i.giphy.com/media/QMHoU66sBXqqLqYvGO/giphy.webp')
-  # browser.go_to('http://motherfuckingwebsite.com/')
 
   loop do
     win.clear
-    display(win, browser, width, height, left, top)
+    display(win, browser, left, top)
     win.refresh
     case win.getch
-    when KEY_UP then top -= 1
-    when KEY_DOWN then top += 1
-    when KEY_LEFT then left - 1
-    when KEY_RIGHT then left + 1
+    when Curses::KEY_UP then top -= height / 8
+    when Curses::KEY_DOWN then top += height / 8
+    when Curses::KEY_LEFT then left -= width / 8
+    when Curses::KEY_RIGHT then left += width / 8
     end
   end
 ensure
